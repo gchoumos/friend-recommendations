2dac4c1e249e834e5aea96a16501259a062b5ca8
gchoumos <g.choumos@gmail.com>
2018-03-11 02:00:11 +0200

    Add some ascii art printing swag

--- run.py
+++ run.py
@@ -6,6 +6,7 @@
 			procs. But I'm not yet sure about this and whether I prefer it or not.
 		-	If using snap is eventually acceptable, add instructions for installation
 			in the README.md file.
+		-	Mention the terminaltables installation as well in the README.md (through pip).
 
 	THOUGHTS:
 		-	I don't think we have to do anything twice as instructed by the assignment
@@ -18,6 +19,8 @@ import snap
 import random
 from data_loader import DataLoader
 from graph import UNGraph
+from terminaltables import AsciiTable
+from terminaltables import SingleTable
 
 nodes = set()
 edges = set()
@@ -47,25 +50,41 @@ def main():
 		print "Computing recommended friendships for node {0} ...".format(node)
 		n_recs = SETTINGS["rec_num"]
 
-		# Print them a bit more nicely than just throwing them on screen
-		print "Common Friends method"
-		print "---------------------"
-		print "Node\t\tScore"
-		for recommendation in un_graph.recommend_friends_CN(node, n_recs):
-			print "{0}\t{1}".format(recommendation[0],recommendation[1])
+		# Show the results for different methods in parallel
+		print "Common Friends method computation ..."
+		rec_common = un_graph.recommend_friends_CN(node,n_recs)
+		print "Jaccard method computation ..."
+		rec_jaccard = un_graph.recommend_friends_CN(node,n_recs)
+		print "Adamic & Adar method computation ..."
+		rec_aa = un_graph.recommend_friends_AA(node,n_recs)
 
-		print "Jaccard method"
-		print "--------------"
-		print "Node\t\tScore"
-		for recommendation in un_graph.recommend_friends_J(node,n_recs):
-			print "{0}\t{1}".format(recommendation[0],recommendation[1])
+		_print_result_tables(node,'Common Friends',rec_common)
+		_print_result_tables(node,'Jaccard',rec_jaccard)
+		_print_result_tables(node,'Adamic & Adar',rec_aa)
 
-		print "Adamic & Adar method"
-		print "--------------------"
-		print "Node\t\tScore"
-		for recommendation in un_graph.recommend_friends_AA(node,n_recs):
-			print "{0}\t{1}".format(recommendation[0],recommendation[1])
 
+def _print_result_tables(node,method,recs):
+	"""
+		This function aims to do a fancy printing of the results into
+		ascii tables. Don't get too excited though. We could also do
+		pipe this to a cowsay for the swag.
+
+		Arguments:
+		- node:	  node id for which we are recommending.
+		- method: name of the method
+		- recs:   actual recommendations of this method
+	"""
+	table_data = [['Rank','NodeID','Score']]
+	for i in range(len(recs)):
+		table_data.append([i+1,recs[i][0],recs[i][1]])
+
+	table = SingleTable(table_data)
+	table.justify_columns[0] = 'right'
+	table.justify_columns[1] = 'right'
+	table.justify_columns[2] = 'right'
+
+	print "\n{0} - Recommendations for Node: {1}".format(method,node)
+	print table.table
 
 if __name__ == '__main__':
     main()
